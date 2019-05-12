40c9ce515a949ca277d0a0dae9d7294a2be5fe7f
Alesander Lopes Gonzaga <alesanderlg@gmail.com>
2019-05-12 19:47:00 -0300

    first configuration of redux

---
+++ src/redux/store.js
@@ -0,0 +1,18 @@
+import { applyMiddleware, combineReducers, createStore } from 'redux';
+import reduxThunk from 'redux-thunk';
+import reducers from './reducer/index';
+import logger from 'redux-logger'
+
+const reducer = combineReducers(reducers);
+
+// Applying middlewares to the store
+const createStoreWithMiddleware = applyMiddleware(reduxThunk, logger)(createStore);
+const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+
+// Creating the store configuration
+const store = createStoreWithMiddleware(
+  reducer,
+  devTools
+);
+
+export default store;
